b4920c67a50d5e1827b31f7c8c43ae2fb8df4185
teevit16881 <72311255+teevit16881@users.noreply.github.com>
2023-04-14 17:31:57 +0700

    Update 6_String and Dictionary.py

--- 6_String and Dictionary.py
+++ 6_String and Dictionary.py
@@ -7,9 +7,42 @@ For our purposes, a valid zip code is any string consisting of exactly 5 digits.
 """
 
 def is_valid_zip(zip_code):
-    """Returns whether the input string is a valid (5 digit) zip code
-    """
+    # Returns whether the input string is a valid (5 digit) zip code
+    
     num_str = ['0','1','2','3','4','5','6','7','8','9']
     if all(num in num_str for num in zip_code) and len(zip_code) == 5:
         return True
     return False
+
+"""
+2.
+A researcher has gathered thousands of news articles. But she wants to focus her attention on articles including a specific word. Complete the function below to help her filter her list of articles.
+
+Your function should meet the following criteria:
+
+Do not include documents where the keyword string shows up only as a part of a larger word. For example, if she were looking for the keyword “closed”, you would not include the string “enclosed.”
+She does not want you to distinguish upper case from lower case letters. So the phrase “Closed the case.” would be included when the keyword is “closed”
+Do not let periods or commas affect what is matched. “It is closed.” would be included when the keyword is “closed”. But you can assume there are no other types of punctuation.
+"""
+
+def word_search(doc_list, keyword):
+    """
+    Takes a list of documents (each document is a string) and a keyword. 
+    Returns list of the index values into the original list for all documents 
+    containing the keyword.
+    """
+    result = []
+    for i in doc_list:
+        
+        # split the sentence in to word list (tokenization)
+        tokens = i.split()
+        
+        # remove symbol then lowercasing all characters (normalization)
+        normalized = [token.rstrip(".,").lower() for token in tokens]
+        
+        #check
+        check = any(keyword in normalized for word in normalized)
+        if check == True:
+            result.append(doc_list.index(i))
+      
+    return result
